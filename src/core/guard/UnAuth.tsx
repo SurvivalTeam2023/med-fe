8f91e3341d8f41d3867b8651e161d6c2af64721b
DatQuoc <datquoc3003@gmail.com>
2023-01-31 23:06:45 +0700

    add first investigate try

--- src/core/guard/UnAuth.tsx
+++ src/core/guard/UnAuth.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import Loading from "components/Loading";
-import { DASHBOARD, TIME_SHEET } from "core/constant";
+import { AUTH_LOGIN, DASHBOARD, NOT_FOUND_PAGE, TIME_SHEET } from "core/constant";
 import { ROLE_ADMIN, ROLE_ARTIST, ROLE_SUBCRIBER } from "core/constant/role";
 
 import { useAppSelector } from "../store";
@@ -10,23 +10,24 @@ import {
   selectUserRoleNames,
   selectUserStore,
 } from "../store/selector";
+import { Role } from "core/interface/role";
 
-const UnAuthGuard: FC = () => {
-  const { isTriedLogin } = useAppSelector(selectUserStore);
-  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+interface AuthWrapperProps {
+  acceptRoles?: Role[];
+}
+const AuthGuard: FC<AuthWrapperProps> = ({ acceptRoles }) => {
+  debugger
   const userRoles = useAppSelector(selectUserRoleNames);
-
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+  const { isTriedLogin } = useAppSelector(selectUserStore);
   if (!isTriedLogin) return <Loading />;
 
-  if (isAuthenticated) {
-    // if (userRoles?.includes(ROLE_ADMIN) || userRoles?.includes()) {
-    //   return <Navigate to={DASHBOARD} replace />;
-    // }
-
-    return <Navigate to={TIME_SHEET} replace />;
+  if (!isAuthenticated) {
+    return <Navigate to={AUTH_LOGIN} replace />;
   }
-
+  if (acceptRoles?.findIndex((item) => userRoles?.includes(item)) === -1) {
+    return <Navigate to={NOT_FOUND_PAGE} replace />;
+  }
   return <Outlet />;
 };
-
-export default UnAuthGuard;
+export default AuthGuard;
