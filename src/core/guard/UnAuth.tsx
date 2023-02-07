a8f65e9e4c7a2ad68928a3db7520175d8ed32fde
DatQuoc <datquoc3003@gmail.com>
2023-02-07 07:45:13 +0700

    add remake login page

--- src/core/guard/UnAuth.tsx
+++ src/core/guard/UnAuth.tsx
@@ -1,33 +1,12 @@
 import { FC } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import Loading from "components/Loading";
-import { AUTH_LOGIN, DASHBOARD, NOT_FOUND_PAGE, TIME_SHEET } from "core/constant";
-import { ROLE_ADMIN, ROLE_ARTIST, ROLE_SUBCRIBER } from "core/constant/role";
-
-import { useAppSelector } from "../store";
-import {
-  selectIsAuthenticated,
-  selectUserRoleNames,
-  selectUserStore,
-} from "../store/selector";
+import { Outlet } from "react-router-dom";
 import { Role } from "core/interface/role";
 
 interface AuthWrapperProps {
   acceptRoles?: Role[];
 }
-const AuthGuard: FC<AuthWrapperProps> = ({ acceptRoles }) => {
-  debugger
-  const userRoles = useAppSelector(selectUserRoleNames);
-  const isAuthenticated = useAppSelector(selectIsAuthenticated);
-  const { isTriedLogin } = useAppSelector(selectUserStore);
-  if (!isTriedLogin) return <Loading />;
-
-  if (!isAuthenticated) {
-    return <Navigate to={AUTH_LOGIN} replace />;
-  }
-  if (acceptRoles?.findIndex((item) => userRoles?.includes(item)) === -1) {
-    return <Navigate to={NOT_FOUND_PAGE} replace />;
-  }
+const UnAuthGuard: FC<AuthWrapperProps> = ({ acceptRoles }) => {
+  console.log("routing wihtou auth");
   return <Outlet />;
 };
-export default AuthGuard;
+export default UnAuthGuard;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,19 +2,31 @@ import styled from "@emotion/styled";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import {
-  Checkbox, FormControl, FormControlLabel, FormGroup, IconButton, InputAdornment, InputLabel, Link as MuiLink,
-  OutlinedInput, Snackbar
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  Link as MuiLink,
+  OutlinedInput,
 } from "@mui/material";
 import "assets/css/app.min.css";
 import "assets/css/bootstrap.min.css";
 import { ILogin } from "core/interface/models";
+import { particles } from "constants/particles";
 import { UserState } from "core/interface/redux";
 import { thunkLogin } from "core/store/thunk";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useCallback } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { loadFull } from "tsparticles";
+import type { Container, Engine } from "tsparticles-engine";
+import { ReactComponent as AuthBackGroundSvg } from "common/icon/auth-bg.svg";
 import { ThunkDispatch } from "redux-thunk";
+import Particles from "react-tsparticles";
 
 export const LinkItem = styled(Link)`
   text-decoration: none;
@@ -49,7 +61,21 @@ const LoginPage: FunctionComponent = () => {
   const [message, setMessage] = React.useState<string>("");
   const dispatch: ThunkDispatch<UserState, null, any> = useDispatch();
   const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
 
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  // const particlesLoaded = useCallback(
+  //   async (container: Container | undefined) => {
+  //     await console.log(container);
+  //   },
+  //   []
+  // );
   const handleMouseDownPassword = (
     event: React.MouseEvent<HTMLButtonElement>
   ) => {
@@ -64,199 +90,190 @@ const LoginPage: FunctionComponent = () => {
     defaultValues,
   });
 
+  const onSubmitHandler: SubmitHandler<ILogin> = async (values: ILogin) => {
+    try {
+      dispatch(thunkLogin(values.username, values.password));
+      setMessage("Login successful");
+    } catch (error) {
+      setMessage("Login fail");
+    }
+  };
 
-    const onSubmitHandler: SubmitHandler<ILogin> = async (values: ILogin) => {
-      try {
-        dispatch(thunkLogin(values.username, values.password));
-        setMessage("Login successful");
-      } catch (error) {
-        setMessage("Login fail");
-      }
-    };
-
-    return (
-      <>
-        <div className="auth-page-wrapper pt-5">
-          <div className="auth-one-bg-position auth-one-bg" id="auth-particles">
-            <div className="bg-overlay"></div>
-
-            <div className="shape">
-              {/* <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
-                <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
-            </svg> */}
-            </div>
+  return (
+    <>
+      <div className="auth-page-wrapper pt-5">
+        <div className="auth-one-bg-position auth-one-bg" id="auth-particles">
+          <div className="bg-overlay"></div>
+          <Particles
+            id="tsparticles"
+            init={particlesInit}
+            options={particles}
+          />
+          <div className="shape">
+            <AuthBackGroundSvg />
           </div>
+        </div>
 
-          <div className="auth-page-content">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="text-center mt-sm-5 mb-4 text-white-50">
-                    <div>
-                      <a href="index.html" className="d-inline-block auth-logo">
-                        <img
-                          src="assets/images/logo-light.png"
-                          alt=""
-                          height="20"
-                        />
-                      </a>
-                    </div>
-                    <p className="mt-3 fs-15 fw-medium">
-                      Premium Admin & Dashboard Template
-                    </p>
+        <div className="auth-page-content">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="text-center mt-sm-5 mb-4 text-white-50">
+                  <div>
+                    <a href="index.html" className="d-inline-block auth-logo">
+                      <img
+                        src="assets/images/logo-light.png"
+                        alt=""
+                        height="20"
+                      />
+                    </a>
                   </div>
+                  <p className="mt-3 fs-15 fw-medium">
+                    Premium Admin & Dashboard Template
+                  </p>
                 </div>
               </div>
+            </div>
 
-              <div className="row justify-content-center">
-                <div className="col-md-8 col-lg-6 col-xl-5">
-                  <div className="card mt-4">
-                    <div className="card-body p-4">
-                      <div className="text-center mt-2">
-                        <h5 className="text-primary">Welcome Back !</h5>
-                        <p className="text-muted">
-                          Sign in to continue to Velzon.
-                        </p>
-                      </div>
-                      <div className="p-2 mt-4">
-                        <form onSubmit={methods.handleSubmit(onSubmitHandler)}>
-                          <div className="mb-3">
-                            {/* <label for="username" className="form-label">Username</label> */}
-                            <input
-                              type="text"
-                              className="form-control"
-                              id="username"
-                              placeholder="Enter username"
-                            />
+            <div className="row justify-content-center">
+              <div className="col-md-8 col-lg-6 col-xl-5">
+                <div className="card mt-4">
+                  <div className="card-body p-4">
+                    <div className="text-center mt-2">
+                      <h5 className="text-primary">Welcome Back !</h5>
+                      <p className="text-muted">
+                        Sign in to continue to Velzon.
+                      </p>
+                    </div>
+                    <div className="p-2 mt-4">
+                      <form onSubmit={methods.handleSubmit(onSubmitHandler)}>
+                        <div className="mb-3">
+                          {/* <label for="username" className="form-label">Username</label> */}
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="username"
+                            placeholder="Enter username"
+                          />
+                        </div>
+
+                        <div className="mb-3">
+                          <div className="float-end">
+                            <a
+                              href="auth-pass-reset-basic.html"
+                              className="text-muted"
+                            >
+                              Forgot password?
+                            </a>
                           </div>
-
-                          <div className="mb-3">
-                            <div className="float-end">
-                              <a
-                                href="auth-pass-reset-basic.html"
-                                className="text-muted"
-                              >
-                                Forgot password?
-                              </a>
-                            </div>
-                            {/* <label className="form-label" for="password-input">
+                          {/* <label className="form-label" for="password-input">
                             Password
                           </label> */}
-                            <div className="position-relative auth-pass-inputgroup mb-3">
-                              <FormControl
-                                sx={{ m: 1, width: "25ch" }}
-                                variant="outlined"
-                              >
-                                <InputLabel htmlFor="outlined-adornment-password">
-                                  Password
-                                </InputLabel>
-                                <OutlinedInput
-                                  id="outlined-adornment-password"
-                                  type={showPassword ? "text" : "password"}
-                                  endAdornment={
-                                    <InputAdornment position="end">
-                                      <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                        edge="end"
-                                      >
-                                        {showPassword ? (
-                                          <VisibilityOff />
-                                        ) : (
-                                          <Visibility />
-                                        )}
-                                      </IconButton>
-                                    </InputAdornment>
-                                  }
-                                  label="Password"
-                                />
-                              </FormControl>
-                            </div>
-                          </div>
-
-                          <div className="form-check">
-                            <FormGroup>
-                              <FormControlLabel
-                                control={<Checkbox defaultChecked />}
-                                label="Label"
-                              />
-                              <FormControlLabel
-                                disabled
-                                control={<Checkbox />}
-                                label="Remember me"
-                              />
-                            </FormGroup>
-                          </div>
-
-                          <div className="mt-4">
-                            <button
-                              className="btn btn-success w-100"
-                              type="submit"
+                          <div className="position-relative auth-pass-inputgroup mb-3">
+                            <FormControl
+                              sx={{ m: 1, width: "25ch" }}
+                              variant="outlined"
                             >
-                              Sign In
+                              <InputLabel htmlFor="outlined-adornment-password">
+                                Password
+                              </InputLabel>
+                              <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={showPassword ? "text" : "password"}
+                                endAdornment={
+                                  <InputAdornment position="end">
+                                    <IconButton
+                                      aria-label="toggle password visibility"
+                                      onClick={handleClickShowPassword}
+                                      onMouseDown={handleMouseDownPassword}
+                                      edge="end"
+                                    >
+                                      {showPassword ? (
+                                        <VisibilityOff />
+                                      ) : (
+                                        <Visibility />
+                                      )}
+                                    </IconButton>
+                                  </InputAdornment>
+                                }
+                                label="Password"
+                              />
+                            </FormControl>
+                          </div>
+                        </div>
+
+                        <div className="form-check">
+                          <FormGroup>
+                            <FormControlLabel
+                              control={<Checkbox />}
+                              label="Remember me"
+                            />
+                          </FormGroup>
+                        </div>
+
+                        <div className="mt-4">
+                          <button
+                            className="btn btn-success w-100"
+                            type="submit"
+                          >
+                            Sign In
+                          </button>
+                        </div>
+
+                        <div className="mt-4 text-center">
+                          <div className="signin-other-title">
+                            <h5 className="fs-13 mb-4 title">Sign In with</h5>
+                          </div>
+                          <div>
+                            <button
+                              type="button"
+                              className="btn btn-primary btn-icon waves-effect waves-light"
+                            >
+                              <i className="ri-facebook-fill fs-16"></i>
+                            </button>
+                            <button
+                              type="button"
+                              className="btn btn-danger btn-icon waves-effect waves-light"
+                            >
+                              <i className="ri-google-fill fs-16"></i>
+                            </button>
+                            <button
+                              type="button"
+                              className="btn btn-dark btn-icon waves-effect waves-light"
+                            >
+                              <i className="ri-github-fill fs-16"></i>
+                            </button>
+                            <button
+                              type="button"
+                              className="btn btn-info btn-icon waves-effect waves-light"
+                            >
+                              <i className="ri-twitter-fill fs-16"></i>
                             </button>
                           </div>
-
-                          <div className="mt-4 text-center">
-                            <div className="signin-other-title">
-                              <h5 className="fs-13 mb-4 title">Sign In with</h5>
-                            </div>
-                            <div>
-                              <button
-                                type="button"
-                                className="btn btn-primary btn-icon waves-effect waves-light"
-                              >
-                                <i className="ri-facebook-fill fs-16"></i>
-                              </button>
-                              <button
-                                type="button"
-                                className="btn btn-danger btn-icon waves-effect waves-light"
-                              >
-                                <i className="ri-google-fill fs-16"></i>
-                              </button>
-                              <button
-                                type="button"
-                                className="btn btn-dark btn-icon waves-effect waves-light"
-                              >
-                                <i className="ri-github-fill fs-16"></i>
-                              </button>
-                              <button
-                                type="button"
-                                className="btn btn-info btn-icon waves-effect waves-light"
-                              >
-                                <i className="ri-twitter-fill fs-16"></i>
-                              </button>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
+                        </div>
+                      </form>
                     </div>
                   </div>
+                </div>
 
-                  <div className="mt-4 text-center">
-                    <p className="mb-0">
-                      Don't have an account ?{" "}
-                      <a
-                        href="auth-signup-basic.html"
-                        className="fw-semibold text-primary text-decoration-underline"
-                      >
-                        {" "}
-                        Signup{" "}
-                      </a>{" "}
-                    </p>
-                  </div>
+                <div className="mt-4 text-center">
+                  <p className="mb-0">
+                    Don't have an account ?{" "}
+                    <a
+                      href="auth-signup-basic.html"
+                      className="fw-semibold text-primary text-decoration-underline"
+                    >
+                      {" "}
+                      Signup{" "}
+                    </a>{" "}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-          <Snackbar
-            open={message.length > 0}
-            autoHideDuration={3000}
-            message={message}
-          />
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
 export default LoginPage;
