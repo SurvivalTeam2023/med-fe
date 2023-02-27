ec0c987501ec6a45b077272b88e51320bbc57f81
hnim1922 <71585912+hnim1922@users.noreply.github.com>
2023-02-27 22:21:02 +0700

    add access_token to baseApi

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,29 +3,14 @@ import { ApiResponse } from "core/interface/api";
 import { User, UserData } from "core/interface/models";
 
 export const getUsersAPI = (
-    token: string | undefined
 ): ApiResponse<User[]> => {
-    const url = `/user/userList `;
-    return CallAPI.get(url,
-        {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
+    const url = `/user/userList`;
+    return CallAPI.get(url)
 }
 
 export const getUserDetailAPI = (
-    token: string | undefined,
     username: string
 ): ApiResponse<UserData> => {
-    const url = `/user/${username} `;
-    console.log(url
-        );
-
-    return CallAPI.get(url,
-        {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
+    const url = `/user/${username}`;
+    return CallAPI.get(url)
 }
