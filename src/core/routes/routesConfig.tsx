68d9c53534d70b2a6c0bf96cb5145bb0a1958662
hnim1922 <71585912+hnim1922@users.noreply.github.com>
2023-03-01 15:36:57 +0700

    rename file UserPage and PlaylistPage

--- src/core/routes/routesConfig.tsx
+++ src/core/routes/routesConfig.tsx
@@ -11,9 +11,9 @@ import LoginPage from "pages/LoginPage/LoginPage";
 import RegisterPage from "pages/RegisterPage/RegisterPage";
 import UserDetail from "pages/UserPage/UserDetail";
 import AudioPage from "pages/AudioPage/audioPage";
-import PlaylistPage from "pages/PlaylistPage/playlistPage";
+import PlaylistPage from "pages/PlaylistPage/PlaylistPage";
 import AudioDetail from "pages/AudioPage/audioDetail";
-import UserPage from "pages/UserPage/userPage";
+import UserPage from "pages/UserPage/UserPage";
 
 export interface SingleRoute {
   path?: string;
