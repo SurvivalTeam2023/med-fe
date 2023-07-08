0d7a7c1ec26c1082a91ef84a6d60427f4c4b633d
tamphamfptu <tampham03112@gmail.com>
2023-07-08 15:58:56 +0700

    fix login url

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,16 +4,14 @@ import { TokenResponse } from "core/interface/redux";
 import { AuthPayload } from "core/interface/models/auth";
 export const getTokenApi = (payload: any) => {
   const { username, password } = payload
-  const url = "/auth/token";
+  const url = "/auth/login/token";
   return CallAPI.post(url, {
     username,
     password,
   });
 };
 
-// export const loginApi = () => {
-//   c
-// }
+
 
 export const registerUserApi = (payload: AuthPayload): ApiResponse<AuthPayload> => {
   const url = "/user/user";
