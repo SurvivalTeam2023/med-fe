89ef9da35a429027bba18e16d09bd8b8ba167dac
Baohuynh <bao.huynh@tpptechnology.com>
2023-08-09 01:16:14 +0700

    add useQuery

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
-import React,  { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './app/store'
-import { Provider } from 'react-redux'
-import SuspenseContent from './containers/SuspenseContent';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import SuspenseContent from "./containers/SuspenseContent";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
 root.render(
   // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <Suspense fallback={<SuspenseContent />}>
-        <Provider store={store}>
-            <App />
-        </Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Suspense>
+    <ReactQueryDevtoolsProduction initialIsOpen />
+  </QueryClientProvider>
   // </React.StrictMode>
 );
 
