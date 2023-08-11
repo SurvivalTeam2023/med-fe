14940d7bd3b88515d0a905333d90cefb8a92b980
Nghia Nguyen <82258622+ntbnghia2308@users.noreply.github.com>
2023-08-11 12:20:26 +0700

    add user

--- src/features/leads/components/AddLeadModalBody.js
+++ src/features/leads/components/AddLeadModalBody.js
@@ -5,7 +5,8 @@ import ErrorText from "../../../components/Typography/ErrorText";
 import InputSelect from "../../../components/Input/InputSelect";
 import { showNotification } from "../../common/headerSlice";
 import { addNewLead } from "../leadSlice";
-import { useCreateUser } from "../../../hooks/user.hook";
+import { createUserAPI } from "../../../Axios/Apis/user/user";
+import { useCreateUser } from "../../../hooks/user.hook.ts";
 
 const INITIAL_LEAD_OBJ = {
   username: "",
@@ -31,26 +32,80 @@ function AddLeadModalBody({ closeModal }) {
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [leadObj, setLeadObj] = useState(INITIAL_LEAD_OBJ);
-
   const { mutate } = useCreateUser();
 
-  const saveNewLead = () => {
-    if (leadObj.first_name.trim() === "")
+  // const saveNewLead = () => {
+  //   if (leadObj.first_name.trim() === "")
+  //     return setErrorMessage("First Name is required!");
+  //   else if (leadObj.email.trim() === "")
+  //     return setErrorMessage("Email id is required!");
+  //   else {
+  //     console.log("leadObj", leadObj);
+  //     let newLeadObj = {
+  //       id: 7,
+  //       email: leadObj.email,
+  //       first_name: leadObj.first_name,
+  //       last_name: leadObj.last_name,
+  //       avatar: "https://reqres.in/img/faces/1-image.jpg",
+  //     };
+  //     dispatch(addNewLead({ newLeadObj }));
+  //     dispatch(showNotification({ message: "New Lead Added!", status: 1 }));
+  //     closeModal();
+  //   }
+  // };
+  const saveNewLead = async () => {
+    if (leadObj.first_name.trim() === "") {
       return setErrorMessage("First Name is required!");
-    else if (leadObj.email.trim() === "")
+    } else if (leadObj.email.trim() === "") {
       return setErrorMessage("Email id is required!");
-    else {
-      console.log("leadObj", leadObj);
-      let newLeadObj = {
-        id: 7,
-        email: leadObj.email,
-        first_name: leadObj.first_name,
-        last_name: leadObj.last_name,
-        avatar: "https://reqres.in/img/faces/1-image.jpg",
-      };
-      dispatch(addNewLead({ newLeadObj }));
-      dispatch(showNotification({ message: "New Lead Added!", status: 1 }));
-      closeModal();
+    } else if (leadObj.username.trim() === "") {
+      return setErrorMessage("Username is required!");
+    } else if (leadObj.password.trim() === "") {
+      return setErrorMessage("Password is required!");
+    } else if (
+      leadObj.repassword.trim() === "" ||
+      leadObj.repassword !== leadObj.password
+    ) {
+      return setErrorMessage("Passwords do not match!");
+    } else if (leadObj.last_name.trim() === "") {
+      return setErrorMessage("Last Name is required!");
+    } else if (leadObj.gender.trim() === "") {
+      return setErrorMessage("Gender is required!");
+    } else if (leadObj.city.trim() === "") {
+      return setErrorMessage("City is required!");
+    } else if (leadObj.address.trim() === "") {
+      return setErrorMessage("Address is required!");
+    } else if (leadObj.dob.trim() === "") {
+      return setErrorMessage("Date of Birth is required!");
+    } else {
+      try {
+        const payload = {
+          username: leadObj.username,
+          email: leadObj.email,
+          password: leadObj.password,
+          repassword: leadObj.repassword,
+          firstName: leadObj.first_name,
+          lastName: leadObj.last_name,
+          gender: leadObj.gender,
+          city: leadObj.city,
+          address: leadObj.address,
+          dob: leadObj.dob,
+        };
+
+        console.log("apiPayload", payload);
+
+        // Assuming mutate function handles the API call internally
+        await mutate(payload);
+
+        // Assuming showNotification and closeModal are actions/functions
+        dispatch(showNotification({ message: "New user Added!", status: 1 }));
+        closeModal();
+      } catch (error) {
+        console.error("Error adding new user:", error);
+        setErrorMessage("Error adding new user. Please try again.");
+      } finally {
+        setLoading(false);
+      }
     }
   };
 
